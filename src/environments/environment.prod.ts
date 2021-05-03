30b50c8d5d25d401ab0aa0eb6039083c7b953f4d
Alexander Gomez Higuita <agomez@payvalida.com>
2021-05-02 20:33:37 -0500

    Config environmentvalues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  gateway: 'https://oqmhfjl3me.execute-api.us-east-1.amazonaws.com',
-  callback: 'http://localhost:4200/callback',
+  api_url: 'https://1f4ref09x5.execute-api.us-east-1.amazonaws.com/prod',
+  gateway: 'https://1f4ref09x5.execute-api.us-east-1.amazonaws.com',
+  callback: 'http://localhost:4200/home',
   domain: 'xalex.us.auth0.com',
-  clientId: 'osYRY7EfBo19260O9MEYE46AuW85kubF',
-  audience: 'xalex'
+  clientId: 'QQ7Bx743PCMzIZ3hDhtZhlZpBU0ky6SP',
+  audience: 'xalexapp'
 };

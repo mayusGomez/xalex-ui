96538e9275927c7353607f3139529cbd45b1d57d
Alexander Gomez Higuita <agomez@payvalida.com>
2021-05-02 22:15:30 -0500

    fix environmet

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   api_url: 'https://1f4ref09x5.execute-api.us-east-1.amazonaws.com/prod',
   gateway: 'https://1f4ref09x5.execute-api.us-east-1.amazonaws.com',
-  callback: 'http://localhost:4200/home',
+  callback: 'https://naughty-poincare-cb4369.netlify.app',
   domain: 'xalex.us.auth0.com',
   clientId: 'QQ7Bx743PCMzIZ3hDhtZhlZpBU0ky6SP',
   audience: 'xalexapp'
